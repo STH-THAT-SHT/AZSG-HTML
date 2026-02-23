4710d8e033e8f6b6c6c30ddc29dc2c79776757df
mvocp <kzstar256@gmail.com>
2026-02-23 09:49:55 +0800

    new version 80% by artificial,not ai

---
+++ static/js/app.js
@@ -0,0 +1,11 @@
+// define the default blocks.
+
+const cstatus = document.getElementById("gameCStatus");
+const history = document.getElementById("gameHistory");
+const opt = document.getElementById("gameOpt");
+const gameDay = document.getElementById("gameDay");
+const gameWeather = document.getElementById("gameWeather");
+const gameBoardcast = document.getElementById("gameBoardcast");
+const gameDirty = document.getElementById("gameDirty");
+
+// init game
